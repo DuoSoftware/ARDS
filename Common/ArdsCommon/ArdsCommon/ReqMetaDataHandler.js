60d2194a463154fb0e35792a6ae89bd1815a4054
heshanindika <k.heshan.indika@gmail.com>
2015-05-13 16:30:16 +0530

    Separate common files

---
+++ Common/ArdsCommon/ArdsCommon/ReqMetaDataHandler.js
@@ -0,0 +1,86 @@
+﻿var util = require('util');
+var redisHandler = require('./RedisHandler.js');
+var infoLogger = require('./InformationLogger.js');
+
+var AddMeataData = function (logKey, metaDataObj, callback) {
+    infoLogger.DetailLogger.log('info', '%s ************************* Start AddMeataData *************************', logKey);
+
+    var key = util.format('ReqMETA:%d:%d:%s:%s:%s', metaDataObj.Company, metaDataObj.Tenant, metaDataObj.Class, metaDataObj.Type, metaDataObj.Category);
+    var tag = ["company_" + metaDataObj.Company, "tenant_" + metaDataObj.Tenant, "class_" + metaDataObj.Class, "type_" + metaDataObj.Type, "category_" + metaDataObj.Category, "objtype_ReqMETA"];
+    
+    var obj = JSON.stringify(metaDataObj);
+    
+    redisHandler.AddObj_T(logKey, key, obj, tag, function (err, result) {
+        infoLogger.DetailLogger.log('info', '%s Finished AddMeataData. Result: %s', logKey, result);
+        callback(err, result);
+    });
+};
+
+var SetMeataData = function (logKey, metaDataObj, callback) {
+    infoLogger.DetailLogger.log('info', '%s ************************* Start SetMeataData *************************', logKey);
+
+    var key = util.format('ReqMETA:%d:%d:%s:%s:%s', metaDataObj.Company, metaDataObj.Tenant, metaDataObj.Class, metaDataObj.Type, metaDataObj.Category);
+    var tag = ["company_" + metaDataObj.Company, "tenant_" + metaDataObj.Tenant, "class_" + metaDataObj.Class, "type_" + metaDataObj.Type, "category_" + metaDataObj.Category, "objtype_ReqMETA"];
+    
+    var obj = JSON.stringify(metaDataObj);
+    
+    redisHandler.SetObj_T(logKey, key, obj, tag, function (err, result) {
+        infoLogger.DetailLogger.log('info', '%s Finished SetMeataData. Result: %s', logKey, result);
+        callback(err, result);
+    });
+};
+
+var GetMeataData = function (logKey, company, tenant, mclass, type, category, callback) {
+    infoLogger.DetailLogger.log('info', '%s ************************* Start GetMeataData *************************', logKey);
+
+    var key = util.format('ReqMETA:%s:%s:%s:%s:%s', company, tenant, mclass, type, category);
+    redisHandler.GetObj(logKey, key, function (err, result) {
+        infoLogger.DetailLogger.log('info', '%s Finished GetMeataData. Result: %s', logKey, result);
+        callback(err, result);
+    });
+};
+
+var SearchMeataDataByTags = function (logKey, tags, callback) {
+    infoLogger.DetailLogger.log('info', '%s ************************* Start SearchMeataDataByTags *************************', logKey);
+
+    if (Array.isArray(tags)) {
+        tags.push("objtype_ReqMETA");
+        redisHandler.SearchObj_T(logKey, tags, function (err, result) {
+        infoLogger.DetailLogger.log('info', '%s Finished SearchMeataDataByTags. Result: %s', logKey, result);
+        callback(err, result);
+        });
+    }
+    else {
+        var e = new Error();
+        e.message = "tags must be a string array";
+        infoLogger.DetailLogger.log('info', '%s Finished SearchMeataDataByTags. Result: tags must be a string array', logKey);
+        callback(e, null);
+    }
+};
+
+var RemoveMeataData = function (logKey, company, tenant, mclass, type, category, callback) {
+    infoLogger.DetailLogger.log('info', '%s ************************* Start RemoveMeataData *************************', logKey);
+
+    var key = util.format('ReqMETA:%s:%s:%s:%s:%s', company, tenant, mclass, type, category);
+    
+    redisHandler.GetObj(logKey, key, function (err, obj) {
+        if (err) {
+            callback(err, "false");
+        }
+        else {
+            var metaDataObj = JSON.parse(obj);
+            var tag = ["company_" + metaDataObj.Company, "tenant_" + metaDataObj.Tenant, "class_" + metaDataObj.Class, "type_" + metaDataObj.Type, "category_" + metaDataObj.Category, "objtype_ReqMETA"];
+    
+            redisHandler.RemoveObj_T(logKey, key, tag, function (err, result) {
+                infoLogger.DetailLogger.log('info', '%s Finished RemoveMeataData. Result: %s', logKey, result);
+                callback(err, result);
+            });
+        }
+    });
+};
+
+module.exports.AddMeataData = AddMeataData;
+module.exports.SetMeataData = SetMeataData;
+module.exports.GetMeataData = GetMeataData;
+module.exports.SearchMeataDataByTags = SearchMeataDataByTags;
+module.exports.RemoveMeataData = RemoveMeataData;
---
+++ Common/ArdsCommon/ArdsCommon/RestClient.js
@@ -0,0 +1,58 @@
+﻿var assert = require('assert');
+var restify = require('restify');
+var request = require('request');
+
+var client = function (url) {
+    return restify.createJsonClient({
+        url: url,
+        version: '~1.0'
+    });
+};
+
+var DoGet = function (url, params, callback) {
+    client(url).get(params, function (err, req, res, obj) {
+        assert.ifError(err);
+        console.log('Server returned: %j', obj);
+        callback(err, res, obj);
+    });
+};
+
+var DoPost = function (url, method, postData, callback) {
+    client(url).post(method, postData, function (err, req, res, obj) {
+        assert.ifError(err);
+        console.log('Server returned: %j', obj);
+        callback(err, res, obj);
+    });
+};
+
+var DoGetSync = function (url, params) {
+    client(url).get(params, function (err, req, res, obj) {
+        assert.ifError(err);
+        console.log('Server returned: %j', obj);
+        return obj;
+    });
+};
+
+var DoPostSync = function (url, postData) {
+    client(url).post(postData, function (err, req, res, obj) {
+        assert.ifError(err);
+        console.log('Server returned: %j', obj);
+        return obj;
+    });
+};
+
+var DoPostDirect = function (serviceurl, postData, callback) {
+    request.post({ url: serviceurl, formData: postData }, function optionalCallback(err, httpResponse, body) {
+        if (err) {
+            return console.error('upload failed:', err);
+        }
+        console.log('Server returned: %j', body);
+        callback(err, httpResponse, body);
+    });
+};
+
+module.exports.DoGet = DoGet;
+module.exports.DoPost = DoPost;
+module.exports.DoGetSync = DoGetSync;
+module.exports.DoPostSync = DoPostSync;
+module.exports.DoPostDirect = DoPostDirect;
